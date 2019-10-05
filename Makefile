5abc3d808398b3de98459fe4caadcca4ad776725
Kotaro7750 <7750koutarou@gmail.com>
2019-10-05 14:29:40 +0900

    ファイル分割した

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 TARGET = kernel.bin
-CFLAGS = -Wall -Wextra -nostdinc -nostdlib -fno-builtin -fno-common
+CFLAGS = -Wall -Wextra -nostdinc -nostdlib -fno-builtin -fno-common 
 LDFLAGS = -Map kernel.map -s -x -T kernel.ld
 
-$(TARGET): main.o
+$(TARGET):main.o fb.o 
 	ld $(LDFLAGS) -o $@ $+
 
 %.o: %.c
@@ -10,8 +10,8 @@ $(TARGET): main.o
 
 run:$(TARGET)
 	mv $(TARGET) ./fs/
-	qemu-system-x86_64 -bios OVMF.fd -hda fat:rw:fs -boot c
+	qemu-system-x86_64 -m 4G -bios OVMF.fd -hda fat:rw:fs -boot c
 clean:
-	rm -f *~ *.o *.map ./fs/$(TARGET) $(TARGET)
+	rm -f *~ *.o *.map ./fs/$(TARGET) $(TARGET) include/*~
 
 .PHONY: clean
--- main.c
+++ main.c
@@ -1,35 +1,11 @@
-#define BG_RED          0
-#define BG_GREEN        255
-#define BG_BLUE         0
+#include "include/fb.h"
 
-struct pixelformat {
-        unsigned char b;
-        unsigned char g;
-        unsigned char r;
-        unsigned char _reserved;
-};
-
-struct framebuffer {
-        struct pixelformat *base;
-        unsigned long long size;
-        unsigned int hr;
-        unsigned int vr;
-};
-
-void start_kernel(void *_t __attribute__ ((unused)), struct framebuffer *fb,
+void start_kernel(void *_t __attribute__ ((unused)), struct framebuffer *_fb,
                   void *_fs_start __attribute__ ((unused)))
 {
-        unsigned int x, y;
-        struct pixelformat *p;
-
-        for (y = 0; y < fb->vr; y++) {
-                for (x = 0; x < fb->hr; x++) {
-                        p = fb->base + (fb->hr * y) + x;
-                        p->r = BG_RED;
-                        p->g = BG_GREEN;
-                        p->b = BG_BLUE;
-                }
-        }
+        fb_init(_fb);
+        set_bg(0, 255, 0);
+        clear_screen();
 
         while (1);
 }
