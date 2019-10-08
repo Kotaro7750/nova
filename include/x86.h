a62ed9e383645c3f07e9ae67ccb429371e9ab796
Kotaro7750 <7750koutarou@gmail.com>
2019-10-08 09:51:08 +0900

    modify

--- include/x86.h
+++ include/x86.h
@@ -49,13 +49,7 @@ unsigned char io_read(unsigned short addr);
 
 void gdt_init(void);
 
-struct segment_descripter {
-  short limit_00_15,base_00_15;
-  char base_16_23,access_right;
-  char limit_16_19_flag,base_24_31;
-};
-
-struct interrupt_descriptor {
+struct __attribute__((packed)) interrupt_descriptor {
   unsigned short offset_00_15;
   unsigned short segment_selector;
   unsigned short ist : 3;
