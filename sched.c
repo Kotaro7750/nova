65bc583e6768497aca46fd20732e98f2e947d3e4
Kotaro7750 <7750koutarou@gmail.com>
2019-10-13 23:18:17 +0900

    modify

--- sched.c
+++ sched.c
@@ -3,10 +3,16 @@
 #include "include/pic.h"
 
 #define SCHED_PERIOD (4 * MS_TO_US)
+#define NUM_TASKS 2
 
-void schedule(void) {
+unsigned long long task_sp[NUM_TASKS];
+volatile unsigned int current_task = 0;
+
+void schedule(unsigned long long current_rsp) {
+  task_sp[current_task] = current_rsp;
+  current_task = (current_task + 1) % NUM_TASKS;
   set_pic_eoi(HPET_INTR_NO);
-  putc('.');
+  asm volatile("mov %[sp], %%rsp" ::[sp] "a"(task_sp[current_task]));
 }
 
 void sched_init(void) { ptimer_setup(SCHED_PERIOD, schedule); }
